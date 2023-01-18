aad8a9354c60365f2d31a6760ade41fb49ac1aeb
Abdirahman A. Osman <abdirahman.osmanthus@gmail.com>
2023-01-18 14:15:55 -0600

    update configs for box

---
+++ nix/machines/box_u/default.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ../common.nix
+  ];
+
+  # allow home manager to manage shell
+  programs.bash = {
+    enable = true;
+    sessionVariables = {
+      EDITOR = "vim";
+    };
+    initExtra = ''
+      # see https://nix-community.github.io/home-manager/index.html#ch-installation
+      . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+      exec ~/.nix-profile/bin/zsh
+    '';
+  };
+  # add home-manager programs to ubuntu application
+  # source: https://github.com/nix-community/home-manager/issues/1439#issuecomment-1106208294
+  home.activation = {
+    linkDesktopApplications = {
+      after = [ "writeBoundary" "createXdgUserDirectories" ];
+      before = [ ];
+      data = ''
+        rm -rf ${config.xdg.dataHome}/"applications/home-manager"
+        mkdir -p ${config.xdg.dataHome}/"applications/home-manager"
+        cp -Lr ${config.home.homeDirectory}/.nix-profile/share/applications/* ${config.xdg.dataHome}/"applications/home-manager/"
+      '';
+    };
+  };
+
+  programs.zsh.initExtra = ''
+    alias wezterm="flatpak run org.wezfurlong.wezterm"
+    '';
+}
---
+++ nix/machines/common.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:
+
+{
+  imports = [
+    #~/nix/programs/wezterm.nix
+    ~/nix/programs/zsh.nix
+    ~/nix/programs/tmux.nix
+    ~/nix/programs/fzf.nix
+    ~/nix/programs/neovim.nix
+    ~/nix/programs/go.nix
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  home.packages = with pkgs; [
+    curl
+    tmux
+    bat
+    git
+    ripgrep
+
+    ngrok
+  ];
+}
