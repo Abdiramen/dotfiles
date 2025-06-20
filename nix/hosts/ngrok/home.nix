6e61e1cab393846fe64dfcb646a390dd01d6ee37
oz <oz@ngrok.com>
2025-06-20 16:08:57 +0000

    update ngrok host

--- nix/hosts/ngrok/home.nix
+++ nix/hosts/ngrok/home.nix
@@ -2,21 +2,32 @@
 
 {
   home.username = "oz";
-  home.homeDirectory = "/home/vimto";
+  home.homeDirectory = "/home/oz";
   home.stateVersion = "24.11";
   nixpkgs.config.allowUnfree = true;
   home.packages = with pkgs; [ ];
   home.file = {};
   home.sessionVariables = {
-    # EDITOR = "emacs";
   };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
- 
   imports = [
-    ../../modules
+    dotfiles/nix/modules
   ];
+
+  programs.bash = {
+    enable = true;
+    initExtra = ''
+      # see https://nix-community.github.io/home-manager/index.html#ch-installation
+      #. "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+      exec ~/.nix-profile/bin/zsh
+    '';
+    profileExtra = ''
+      PATH=~/.nix-profile/bin:$PATH
+    '';
+  };
+
   wezterm-remote = {
     enable = true;
   };
